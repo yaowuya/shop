0059eea57ab30d5843a8776f7b5d5611b48e6831
yaowuya <zrfyaowuya@126.com>
2019-12-15 13:59:14 +0800

    first commit

---
+++ shopping-admin/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/essential',
+    '@vue/standard'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'camelcase':0,
+    'key-spacing':0,
+    "indent": 0
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+}
